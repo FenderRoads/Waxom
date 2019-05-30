1e0001bddcef4c6ec3c17ed7fb8531931e93af12
FenderRoads <48343809+FenderRoads@users.noreply.github.com>
2019-05-30 18:19:04 +0400

    Preloader and button up

--- app/js/common.js
+++ app/js/common.js
@@ -1,46 +1,55 @@
 $(function() {
-
-$('.carousel1').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive: {
-    0: {
-      items: 1
+  $(".carousel1").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      }
     }
-  }
-})
+  });
 
-$('.carousel2').owlCarousel({
-  loop:true,
-  margin:20,
-  nav:true,
-  dots: false,
-  responsive: {
-    0: {
-      items: 1
-    },
-    750: {
-      items: 2
-    },
-    1000: {
-      items: 3
+  $(".carousel2").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      750: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
     }
-  }
-})
+  });
 
-$('.bxslider').bxSlider({
-  mode: 'horizontal',
-  captions: true
-});
+  $(".hamburger").on("click", function() {
+    $(this).toggleClass("is-active");
+    $(".drop-down-menu").toggleClass("menu-active");
+  });
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 900) {
+      $(".top").addClass("active");
+    } else {
+      $(".top").removeClass("active");
+    }
+  });
 
-  
+  $(".top").on("click", function() {
+    $("html, body")
+      .stop()
+      .animate({ scrollTop: 0 }, "slow", "swing");
+  });
 
-
-$('.hamburger').on('click', function(){
-  $(this).toggleClass('is-active')
-  $('.drop-down-menu').toggleClass('menu-active')
-})
-
+  $(window).on("load", function() {
+    $(".preloader")
+      .delay(1000)
+      .fadeOut("slow");
+  });
 });
